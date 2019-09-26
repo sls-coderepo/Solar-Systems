60aaa7e4343c017c57b90e8eebb7c8de9df05fdc
unknown <[shrestha_sl@hotmail.com]>
2019-09-25 20:30:55 -0500

    list of planet arrays

--- src/arrayMethod.js
+++ src/arrayMethod.js
@@ -1,1 +1,9 @@
 const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+
+
+const planetList = document.querySelector("#planets")
+ planets.forEach(planet => {
+     planetList.innerHTML += `<section>${planet}</section>`
+     
+ })
+
